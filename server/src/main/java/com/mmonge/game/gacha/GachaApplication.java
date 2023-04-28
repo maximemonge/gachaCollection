144d7c5964765427c97eb115e4862fbf9e2001a8
Maxime Monge <maxime.monge@yahoo.fr>
2023-04-28 23:58:31 +0200

    init

---
+++ server/src/main/java/com/mmonge/game/gacha/GachaApplication.java
@@ -0,0 +1,13 @@
+package com.mmonge.game.gacha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachaApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/mmonge/game/gacha/GachaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmonge.game.gacha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
